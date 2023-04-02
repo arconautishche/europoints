20e503a8ebbd6197b4f1947ee740274a25a967db
arconautishche <anton.iar@hey.com>
2023-04-02 10:41:12 +0200

    shoing "my watch parties"

--- lib/pointex_web/live/home.ex
+++ lib/pointex_web/live/home.ex
@@ -8,11 +8,14 @@ defmodule PointexWeb.Home do
     <div class="flex flex-col md:flex-row justify-stretch gap-4 md:gap-8 p-4">
       <.my_watch_parties :if={@user} my_watch_parties={@my_watch_parties}/>
 
-      <div class={["w-full flex flex-col md:-mt-10 gap-4 max-w-md bg-white shadow rounded p-4 sm:p-6 md:p-8", if(@user, do: "", else: "mx-auto")]}>
-        <.button>Join Watch Party</.button>
-        <.button navigate_to={~p"/wp/new"} kind="secondary">
-          New Watch Party
-        </.button>
+      <div class="flex flex-col gap-2 w-full">
+        <h1 class="uppercase text-gray-400">Don't have one yet?</h1>
+        <div class={["w-full flex flex-col gap-4 max-w-md bg-white shadow rounded p-4 sm:p-6 md:p-8", if(@user, do: "", else: "mx-auto")]}>
+          <.button>Join Watch Party</.button>
+          <.button navigate_to={~p"/wp/new"} kind="secondary">
+            New Watch Party
+          </.button>
+        </div>
       </div>
     </div>
     """
@@ -25,12 +28,24 @@ defmodule PointexWeb.Home do
 
   defp my_watch_parties(assigns) do
     ~H"""
-    <div class="w-full flex flex-col md:-mt-10 gap-4 max-w-md bg-white shadow rounded p-4 sm:p-6 md:p-8">
+    <div class="w-full flex flex-col gap-2">
       <h1 class="uppercase text-gray-400">My Watch Parties</h1>
 
       <div :for={wp <- @my_watch_parties}>
-        <%= wp.name %>
+        <.watch_party_card watch_party={wp} />
       </div>
+    </div>
+    """
+  end
+
+  defp watch_party_card(assigns) do
+    ~H"""
+    <div class="w-full flex flex-col gap-4 max-w-md bg-white shadow rounded p-4 px-6 cursor-pointer transition hover:bg-amber-100/50 hover:scale-105">
+      <div class="flex gap-4 items-top">
+        <.icon name="hero-user-group" class="text-sky-700 h-8 w-8" />
+        <h2 class="text-gray-600"><%= @watch_party.name %></h2>
+      </div>
+
     </div>
     """
   end
