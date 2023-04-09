b5278cf78621b1fe4e86d0a62a1e693a9d362bff
arconautishche <anton.iar@hey.com>
2023-04-09 14:31:04 +0200

    reset eventstore

--- lib/pointex/commanded/util.ex
+++ lib/pointex/commanded/util.ex
@@ -1,5 +1,4 @@
 defmodule Pointex.Commanded.Util do
-  alias Pointex.Commanded.EventStore
   alias Pointex.Repo
 
   def reset_projection(name) do
@@ -11,6 +10,14 @@ defmodule Pointex.Commanded.Util do
 
     Repo.query("delete from projection_versions where projection_name = '#{name}'") |> IO.inspect()
 
-    EventStore.delete_subscription("$all", name) |> IO.inspect()
+    Pointex.Commanded.EventStore.delete_subscription("$all", name) |> IO.inspect()
+  end
+
+  def reset_eventstore! do
+    config = Pointex.Commanded.EventStore.config()
+
+    {:ok, conn} = Postgrex.start_link(config)
+
+    EventStore.Storage.Initializer.reset!(conn, config) |> IO.inspect()
   end
 end
