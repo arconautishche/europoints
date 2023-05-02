1bdddb0b029dc6566d29721ef65c296075ddaa80
arconautishche <anton.iar@hey.com>
2023-05-02 19:59:40 +0200

    grab song details from static dictionary

--- lib/pointex_web/live/watch_party/components/song_components.ex
+++ lib/pointex_web/live/watch_party/components/song_components.ex
@@ -1,27 +1,30 @@
 defmodule PointexWeb.WatchParty.SongComponents do
   use PointexWeb, :html
+  import Pointex.Model.ReadModels.Shows
 
   attr :song, :map, required: true
   attr :class, :any, default: ""
 
   def description(assigns) do
     ~H"""
-    <div class={"flex gap-2 grow " <> @class}>
-      <div class="text-4xl">
-        <%= @song.details["flag"] %>
-      </div>
-      <div class="flex flex-col gap-2">
-        <div class="text-xl pt-1">
-          <%= @song.details["country"] %>
+    <div class={"relative grow" <> @class}>
+      <div class="relative flex gap-2 z-20">
+        <div class="text-4xl">
+          <%= song_details(@song.id).flag %>
         </div>
         <div class="flex flex-col gap-2">
-          <div class="flex gap-2 items-center text-black/40">
-            <.icon name="hero-microphone" class="text-sky-400/50 w-4 h-4" />
-            <%= @song.details["artist"] %>
+          <div class="text-xl pt-1">
+            <%= @song.id %>
           </div>
-          <div class="flex gap-2 items-center text-black/40">
-            <.icon name="hero-musical-note" class="text-sky-400/50 w-4 h-4" />
-            <%= @song.details["song"] %>
+          <div class="flex flex-col gap-2">
+            <div class="flex gap-2 items-center text-black/60">
+              <.icon name="hero-microphone" class="text-sky-400/70 w-4 h-4" />
+              <%= song_details(@song.id).artist %>
+            </div>
+            <div class="flex gap-2 items-center text-black/60">
+              <.icon name="hero-musical-note" class="text-sky-400/70 w-4 h-4" />
+              <%= song_details(@song.id).song %>
+            </div>
           </div>
         </div>
       </div>
