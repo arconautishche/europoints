d08bda6fc0e8ee49b5ea4f760b8794c2e882fd7e
arconautishche <anton.iar@fastmail.com>
2024-04-03 19:49:30 +0200

    user registration & login

--- lib/pointex_web/controllers/login_controller.ex
+++ lib/pointex_web/controllers/login_controller.ex
@@ -1,26 +1,28 @@
 defmodule PointexWeb.LoginController do
-  alias Pointex.Model.ReadModels
-  alias Pointex.Model.Commands
   use PointexWeb, :controller
+  alias Pointex.Europoints
+  alias Pointex.Europoints.Account
 
   def register(conn, %{"register_params" => register_params, "return_to" => return_to}) do
-    case register(register_params) do
-      :ok ->
+    name = Map.get(register_params, "user_name", "")
+
+    case Account.register(name) do
+      {:ok, account} ->
         conn
         |> Plug.Conn.put_session("user", %{
-          user_id: register_params["user_id"],
-          user_name: register_params["user_name"]
+          user_id: account.id,
+          user_name: account.name
         })
         |> redirect(to: return_to)
 
-      {:error, :invalid_params} ->
+      {:error, _} ->
         redirect(conn, to: ~p"/register")
     end
   end
 
   def login(conn, %{"login_params" => login_params, "return_to" => return_to}) do
-    case find_existing_participant(login_params) do
-      %{id: id, name: name} ->
+    case Europoints.get(Account, login_params["user_id"]) do
+      {:ok, %{id: id, name: name}} ->
         conn
         |> Plug.Conn.put_session("user", %{
           user_id: id,
@@ -28,7 +30,7 @@ defmodule PointexWeb.LoginController do
         })
         |> redirect(to: return_to)
 
-      :not_found ->
+      _ ->
         redirect(conn, to: ~p"/login")
     end
   end
@@ -37,25 +39,5 @@ defmodule PointexWeb.LoginController do
     conn
     |> Plug.Conn.put_session("user", nil)
     |> redirect(to: ~p"/")
-  end
-
-  defp register(register_params) do
-    user_id = Map.get(register_params, "user_id", "")
-    user_name = Map.get(register_params, "user_name", "")
-
-    case Commands.RegisterParticipant.dispatch_new(%{
-           pool_id: "default",
-           id: user_id,
-           name: user_name
-         }) do
-      :ok -> :ok
-      {:errors, _} -> {:error, :invalid_params}
-    end
-  end
-
-  defp find_existing_participant(login_params) do
-    user_id = Map.get(login_params, "user_id", "")
-
-    ReadModels.Participants.find(user_id)
   end
 end
