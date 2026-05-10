749ec6d482a5238f7cdcb4f53d93d30d2208bdd3
arconautishche <anton.iar@fastmail.com>
2026-05-10 13:26:31 +0200

    fix: non-final votes included in total scores

--- lib/pointex/europoints/participant/top_10_with_points.ex
+++ lib/pointex/europoints/participant/top_10_with_points.ex
@@ -3,6 +3,11 @@ defmodule Pointex.Europoints.Participant.Top10WithPoints do
   alias Pointex.Europoints.Participant.Voting
 
   @impl true
+  def load(_query, _opts, _context) do
+    [:top_10]
+  end
+
+  @impl true
   def calculate(participants, _opts, _context) do
     Enum.map(participants, &Voting.map_points_to_top_10(&1.top_10))
   end
